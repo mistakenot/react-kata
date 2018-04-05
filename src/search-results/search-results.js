027de0b7e12de0239834d946afc8c5fc83030be9
Charlie <the_charlie_daly@hotmail.co.uk>
2018-04-06 00:33:28 +0100

    Search results displays name
    - Name displayed for each child item

--- src/search-results/search-results.js
+++ src/search-results/search-results.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 export const styles = {
-  searchResultsItem: 'search-results-item'
+  searchResultsItem: 'search-results-item',
+  searchResultsItemName: 'search-results-item-name'
 };
 
 const SearchResults = props => (
   <div> 
   { props.resultItems.map((item, i) =>
-    <div key={i} className={styles.searchResultsItem} ></div>) }
+    <div key={i} className={styles.searchResultsItem} >
+      <p className={styles.searchResultsItemName}>{ item.Name }</p>
+    </div>) }
   </div>
 );
 
--- src/search-results/search-results.test.js
+++ src/search-results/search-results.test.js
@@ -11,12 +11,27 @@ describe('Search Results', () => {
     shallow(<SearchResults resultItems={items} />);
   });
 
-  it('displays a representation of each child item', () => {
+  it('displays a representation of each result item', () => {
     const items = [0,1,2];
-    const wrapper = shallow(<SearchResults resultItems={items} />)
+    const wrapper = shallow(<SearchResults resultItems={items} />);
     const resultItemCount = wrapper.find('.' + styles.searchResultsItem).length;
     
     expect(resultItemCount).toEqual(items.length);
   });
+
+  it('displays a name for each result item', () => {
+    const items = [
+      {Name: 'bob'},
+      {Name: 'alice'}
+    ]
+    const wrapper = shallow(<SearchResults resultItems={items} />);
+    const resultItemNames = wrapper
+      .find('.' + styles.searchResultsItemName + ' p')
+      .map(resultItem => resultItem.text());
+
+    const expected = items.map(i => i.Name);
+
+    expect(resultItemNames).toEqual(expected);
+  });  
 
 });
